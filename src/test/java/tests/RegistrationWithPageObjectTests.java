a87884b34ee93d4488d05ff89b32f5f29a3d8f26
Elena Tretyak <ieo_88@mail.ru>
2025-01-04 07:45:43 +0300

    added tag @Tag("demoqa") to RegistrationWithPageObjectTests

--- src/test/java/tests/RegistrationWithPageObjectTests.java
+++ src/test/java/tests/RegistrationWithPageObjectTests.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
@@ -12,6 +13,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationWithPageObjectTests extends TestBase {
+    @Tag("demoqa")
     @Test
 
     void successfulRegistrationTest () {
